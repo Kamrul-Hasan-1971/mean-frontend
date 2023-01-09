e13e3cdfec36f4c99f4e11e2260d84ce9fa00261
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-01-09 12:40:31 +0600

    Mean frontend initialized

---
+++ src/app/auth/auth-data.model.ts
@@ -0,0 +1,11 @@
+export interface AuthDataSignUp {
+  email: string;
+  password: string;
+  confirmPassword: string
+}
+
+export interface AuthDataLogin {
+  email: string;
+  password: string;
+}
+
