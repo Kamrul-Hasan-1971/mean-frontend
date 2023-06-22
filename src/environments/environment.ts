3675778ade27a77c91a093f6b490f587bba468a1
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-06-22 10:34:35 +0600

    Change the api url for dev environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: "http://localhost:3000/api",
-  apiUrl:"https://mean.up.railway.app/api"
+  // apiUrl:"https://mean.up.railway.app/api"
+     apiUrl:"https://tense-duck-culottes.cyclic.app"
 };
 
 /*
