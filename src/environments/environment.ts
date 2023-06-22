6c03902064a0202451fc9e1ef3057e7613d061c1
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-06-22 13:07:41 +0600

    add .npmrc file to reolve failing issue during deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 
 export const environment = {
   production: false,
-    // apiUrl: "http://localhost:3000/api",
+     apiUrl: "http://localhost:3000/api",
     // apiUrl:"https://mean.up.railway.app/api"
     // apiUrl:"https://tense-duck-culottes.cyclic.app"
-    apiUrl:"https://tense-duck-culottes.cyclic.app"
+    // apiUrl:"https://tense-duck-culottes.cyclic.app"
 };
 
 
