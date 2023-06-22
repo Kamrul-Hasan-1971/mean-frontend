5016a0981b30f49e5ea676ef37fec115a584c7ab
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-06-22 10:32:24 +0600

    Remove unncessary codes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, } from "@angular/core";
-// import { Subscription } from "rxjs";
-
 import { AuthService } from "./auth/auth.service";
-// import { ErrorService } from "./error/error.service";
 
 @Component({
   selector: "app-root",
@@ -10,25 +7,12 @@ import { AuthService } from "./auth/auth.service";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  // hasError = false;
-  // private errorSub: Subscription;
-  // totalPosts = 0;
-  // postsPerPage = 2;
-  // currentPage = 1;
-  // pageSizeOptions = [1, 2, 5, 10];
   constructor(
     private authService: AuthService,
-    // private errorService: ErrorService
   ) {}
 
   ngOnInit() {
     this.authService.autoAuthUser();
-    // this.errorSub = this.errorService.getErrorListener().subscribe(
-    //   message => this.hasError = message !== null
-    // );
   }
 
-  // ngOnDestroy() {
-  //   this.errorSub.unsubscribe();
-  // }
 }
